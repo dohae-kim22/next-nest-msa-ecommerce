b3ca70f529d2c69fc99858d9bfe42b3eb6d2025d
DODO <dohae.kim22@gmail.com>
2025-04-17 21:27:02 +0200

    Feat(Auth): Add Auth Controller

--- apps/back-api/apps/user/src/app.module.ts
+++ apps/back-api/apps/user/src/app.module.ts
@@ -1,8 +1,30 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        HTTP_PORT: Joi.number().required(),
+        DB_URL: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.getOrThrow<string>('DB_URL'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    UserModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
--- apps/back-api/apps/user/src/auth/auth.controller.ts
+++ apps/back-api/apps/user/src/auth/auth.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register-dto';
 
-@Controller()
+@Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  registerUser(token: string, @Body() registerDto: RegisterDto) {
+    if (!token) {
+      throw new BadRequestException('Token is required');
+    }
+    return this.authService.register(token, registerDto);
+  }
 }
